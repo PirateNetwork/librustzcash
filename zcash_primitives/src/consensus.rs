7297f01b50b7e2f72a55f3db6550245da63d742c
Cryptoforge <cryptoforge.cc@gmail.com>
2022-07-04 17:00:01 -0700

    Add Pirate as a network option

--- zcash_primitives/src/consensus.rs
+++ zcash_primitives/src/consensus.rs
@@ -240,6 +240,51 @@ impl Parameters for MainNetwork {
     }
 }
 
+/// Marker struct for the Pirate production network.
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub struct PirateNetwork;
+
+pub const PIRATE_NETWORK: PirateNetwork = PirateNetwork;
+
+impl Parameters for PirateNetwork {
+    fn activation_height(&self, nu: NetworkUpgrade) -> Option<BlockHeight> {
+        match nu {
+            NetworkUpgrade::Overwinter => Some(BlockHeight(0)),
+            NetworkUpgrade::Sapling => Some(BlockHeight(152_855)),
+            NetworkUpgrade::Blossom => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Heartwood => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Canopy => Some(BlockHeight(4_000_000_000)),
+            NetworkUpgrade::Nu5 => Some(BlockHeight(4_000_000_000)),
+            #[cfg(feature = "zfuture")]
+            NetworkUpgrade::ZFuture => None,
+        }
+    }
+
+    fn coin_type(&self) -> u32 {
+        constants::piratenet::COIN_TYPE
+    }
+
+    fn hrp_sapling_extended_spending_key(&self) -> &str {
+        constants::piratenet::HRP_SAPLING_EXTENDED_SPENDING_KEY
+    }
+
+    fn hrp_sapling_extended_full_viewing_key(&self) -> &str {
+        constants::piratenet::HRP_SAPLING_EXTENDED_FULL_VIEWING_KEY
+    }
+
+    fn hrp_sapling_payment_address(&self) -> &str {
+        constants::piratenet::HRP_SAPLING_PAYMENT_ADDRESS
+    }
+
+    fn b58_pubkey_address_prefix(&self) -> [u8; 2] {
+        constants::piratenet::B58_PUBKEY_ADDRESS_PREFIX
+    }
+
+    fn b58_script_address_prefix(&self) -> [u8; 2] {
+        constants::piratenet::B58_SCRIPT_ADDRESS_PREFIX
+    }
+}
+
 /// Marker struct for the test network.
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 pub struct TestNetwork;
@@ -295,6 +340,7 @@ impl Parameters for TestNetwork {
 pub enum Network {
     MainNetwork,
     TestNetwork,
+    PirateNetwork,
 }
 
 memuse::impl_no_dynamic_usage!(Network);
@@ -304,6 +350,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.activation_height(nu),
             Network::TestNetwork => TEST_NETWORK.activation_height(nu),
+            Network::PirateNetwork => PIRATE_NETWORK.activation_height(nu),
         }
     }
 
@@ -311,6 +358,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.coin_type(),
             Network::TestNetwork => TEST_NETWORK.coin_type(),
+            Network::PirateNetwork => PIRATE_NETWORK.coin_type(),
         }
     }
 
@@ -325,6 +373,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.hrp_sapling_extended_spending_key(),
             Network::TestNetwork => TEST_NETWORK.hrp_sapling_extended_spending_key(),
+            Network::PirateNetwork => PIRATE_NETWORK.hrp_sapling_extended_spending_key(),
         }
     }
 
@@ -332,6 +381,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.hrp_sapling_extended_full_viewing_key(),
             Network::TestNetwork => TEST_NETWORK.hrp_sapling_extended_full_viewing_key(),
+            Network::PirateNetwork => PIRATE_NETWORK.hrp_sapling_extended_full_viewing_key(),
         }
     }
 
@@ -339,6 +389,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.hrp_sapling_payment_address(),
             Network::TestNetwork => TEST_NETWORK.hrp_sapling_payment_address(),
+            Network::PirateNetwork => PIRATE_NETWORK.hrp_sapling_payment_address(),
         }
     }
 
@@ -346,6 +397,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.b58_pubkey_address_prefix(),
             Network::TestNetwork => TEST_NETWORK.b58_pubkey_address_prefix(),
+            Network::PirateNetwork => PIRATE_NETWORK.b58_pubkey_address_prefix(),
         }
     }
 
@@ -353,6 +405,7 @@ impl Parameters for Network {
         match self {
             Network::MainNetwork => MAIN_NETWORK.b58_script_address_prefix(),
             Network::TestNetwork => TEST_NETWORK.b58_script_address_prefix(),
+            Network::PirateNetwork => PIRATE_NETWORK.b58_script_address_prefix(),
         }
     }
 }
