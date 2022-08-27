bf291921fe1f8a223d2a76c392d6bd97ebc3fd19
Cryptoforge <cryptoforge.cc@gmail.com>
2022-08-27 10:14:04 -0700

    Remove PirateNetwork and replace zcash constants with Pirate constants

--- zcash_primitives/src/constants/testnet.rs
+++ zcash_primitives/src/constants/testnet.rs
@@ -1,16 +1,15 @@
-//! Constants for the Zcash test network.
+//! Constants for the Pirate test network.
 
-/// The testnet coin type for ZEC, as defined by [SLIP 44].
+/// The testnet coin type for ARRR, as defined by [SLIP 44].
 ///
 /// [SLIP 44]: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-pub const COIN_TYPE: u32 = 1;
+pub const COIN_TYPE: u32 = 141;
 
 /// The HRP for a Bech32-encoded testnet [`ExtendedSpendingKey`].
 ///
 /// Defined in [ZIP 32].
 ///
 /// [`ExtendedSpendingKey`]: crate::zip32::ExtendedSpendingKey
-/// [ZIP 32]: https://github.com/zcash/zips/blob/master/zip-0032.rst
 pub const HRP_SAPLING_EXTENDED_SPENDING_KEY: &str = "secret-extended-key-test";
 
 /// The HRP for a Bech32-encoded testnet [`ExtendedFullViewingKey`].
@@ -18,7 +17,6 @@ pub const HRP_SAPLING_EXTENDED_SPENDING_KEY: &str = "secret-extended-key-test";
 /// Defined in [ZIP 32].
 ///
 /// [`ExtendedFullViewingKey`]: crate::zip32::ExtendedFullViewingKey
-/// [ZIP 32]: https://github.com/zcash/zips/blob/master/zip-0032.rst
 pub const HRP_SAPLING_EXTENDED_FULL_VIEWING_KEY: &str = "zxviewtestsapling";
 
 /// The HRP for a Bech32-encoded testnet [`PaymentAddress`].
@@ -26,15 +24,14 @@ pub const HRP_SAPLING_EXTENDED_FULL_VIEWING_KEY: &str = "zxviewtestsapling";
 /// Defined in section 5.6.4 of the [Zcash Protocol Specification].
 ///
 /// [`PaymentAddress`]: crate::sapling::PaymentAddress
-/// [Zcash Protocol Specification]: https://github.com/zcash/zips/blob/master/protocol/protocol.pdf
 pub const HRP_SAPLING_PAYMENT_ADDRESS: &str = "ztestsapling";
 
 /// The prefix for a Base58Check-encoded testnet [`TransparentAddress::PublicKey`].
 ///
 /// [`TransparentAddress::PublicKey`]: crate::legacy::TransparentAddress::PublicKey
-pub const B58_PUBKEY_ADDRESS_PREFIX: [u8; 2] = [0x1d, 0x25];
+pub const B58_PUBKEY_ADDRESS_PREFIX: [u8; 1] = [0x00];
 
 /// The prefix for a Base58Check-encoded testnet [`TransparentAddress::Script`].
 ///
 /// [`TransparentAddress::Script`]: crate::legacy::TransparentAddress::Script
-pub const B58_SCRIPT_ADDRESS_PREFIX: [u8; 2] = [0x1c, 0xba];
+pub const B58_SCRIPT_ADDRESS_PREFIX: [u8; 1] = [0x05];
